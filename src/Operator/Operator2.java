0a61ba71d8fa07de68fd9566535d4a606ad19a34
clolc10 <clolc410@gmail.com>
2025-03-03 15:13:54 +0900

    java-start study

---
+++ src/Operator/Operator2.java
@@ -0,0 +1,9 @@
+package Operator;
+
+public class Operator2 {
+    public static void main(String[] args) {
+
+        String result1 = "hello" +"world";
+        System.out.println(result1);
+    }
+}
---
+++ src/variable/Var3.java
@@ -0,0 +1,11 @@
+package variable;
+
+public class Var3 {
+    public static void main(String[] args) {
+        int b; //변수 선언
+        b = 10; //변수 초기화
+        System.out.println(b);
+        b = 15;
+        System.out.println(b);
+    }
+}
